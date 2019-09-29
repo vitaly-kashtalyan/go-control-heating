70e398ec8e9a392cb06c0744b4284158365144ee
Vitaly Kashtalyan <vitaly.kashtalyan@gmail.com>
2019-09-29 18:39:08 +0300

    Added temporary project

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/vitaly-kashtalyan/go-control-heating
+
+go 1.13
+
+require (
+	github.com/jasonlvhit/gocron v0.0.0-20190920201010-985d45da66c5
+	github.com/stretchr/testify v1.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jasonlvhit/gocron"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	ENABLE  = "on"
+	DISABLE = "off"
+)
+
+type Relays struct {
+	Relay []relay `json:"relays"`
+}
+
+type relay struct {
+	Pin         int     `json:"pin"`
+	DecSensor   string  `json:"dec"`
+	Temperature float32 `json:"temperature"`
+	Relay       int     `json:"relay"`
+	Name        string  `json:"name"`
+}
+
+type Sensors struct {
+	Status  int      `json:"status"`
+	Message string   `json:"message"`
+	Data    []sensor `json:"data"`
+}
+
+type sensor struct {
+	Pin         int       `json:"pin"`
+	DecSensor   string    `json:"dec"`
+	Temperature float32   `json:"temperature"`
+	Humidity    float32   `json:"humidity"`
+	CreatedAt   time.Time `json:"date"`
+}
+
+func main() {
+	s := gocron.NewScheduler()
+	s.Every(3).Minutes().Do(manageRelays)
+	<-s.Start()
+}
+
+func manageRelays() {
+	sensors := Sensors{}
+	if err := getJSON("http://192.168.0.8:8084", &sensors); err == nil {
+		if sensors.Message == "OK" {
+			for _, v := range sensors.Data {
+				relayId, temperature := getRuleByPinAndDec(v.Pin, v.DecSensor)
+				if relayId != -1 || temperature != -1 {
+					statusRelay := DISABLE
+					if v.Temperature < temperature {
+						statusRelay = ENABLE
+					}
+					_ = sendRelayStatus(relayId, statusRelay)
+				}
+			}
+		}
+	} else {
+		fmt.Printf("manageRelays: %v", err)
+	}
+}
+
+func sendRelayStatus(relayId int, status string) error {
+	url := fmt.Sprintf("http://192.168.0.8:8082/relays/%s/%d", status, relayId)
+	fmt.Printf("GET: %s\n", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("cannot fetch URL %q: %v", url, err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected http GET status: %s", resp.Status)
+	}
+
+	return nil
+}
+
+func getRuleByPinAndDec(pin int, dec string) (int, float32) {
+	jsonFile, err := os.Open("rules.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var relays Relays
+	_ = json.Unmarshal(byteValue, &relays)
+
+	for i := 0; i < len(relays.Relay); i++ {
+		if pin == relays.Relay[i].Pin && dec == relays.Relay[i].DecSensor {
+			return relays.Relay[i].Relay, relays.Relay[i].Temperature
+		}
+	}
+	return -1, -1
+}
+
+func getJSON(url string, result interface{}) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("cannot fetch URL %q: %v", url, err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected http GET status: %s", resp.Status)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(result)
+	if err != nil {
+		return fmt.Errorf("cannot decode JSON: %v", err)
+	}
+	return nil
+}
