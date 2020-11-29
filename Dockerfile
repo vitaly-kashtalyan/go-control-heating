27fb075662cb8106e1f961389e961b188a3cb4c6
Vitaly Kashtalyan <vitaly.kashtalyan@gmail.com>
2020-11-29 10:09:52 +0300

    Removed rules and refactored main logic

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
-FROM golang:1.13-alpine
+FROM golang:1.15-alpine AS build
 WORKDIR /app
 COPY go.mod go.sum ./
 RUN go mod download
-COPY . .
+COPY main.go .
 RUN go build -o main .
+
+FROM alpine:latest
+WORKDIR /root
+COPY --from=build /app/main .
 CMD ["./main"]
--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-docker-compose up --build -d
+#docker-compose up --build -d
+
+#unset VARIABLE_NAME
+
+export RELAYS_SERVICE_HOST="192.168.0.8:8082"
+export SENSORS_SERVICE_HOST="192.168.0.8:8084"
+export RULES_SERVICE_HOST="192.168.0.8:8071"
+
+source ~/.zshrc
